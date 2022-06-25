2ce196a8b2eab249a0e5f7a078f4991539bf25ed
rkdwlss2 <rkdwlss2@gmail.com>
2022-06-25 12:07:01 +0900

    test

--- src/main/java/com/cos/controllerdemo/ControllerDemoApplication.java
+++ src/main/java/com/cos/controllerdemo/ControllerDemoApplication.java
@@ -3,7 +3,7 @@ package com.cos.controllerdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //test
 public class ControllerDemoApplication {
 
     public static void main(String[] args) {
